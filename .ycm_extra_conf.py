328eba340751470241f15c3dcf1ac63c0cda53fa
qw9002 <1043102712@qq.com>
2019-10-25 11:27:57 +0800

    fix

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -32,14 +32,15 @@ flags = [
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
 # a "-std=<something>".
 # For a C project, you would set this to something like 'c99' instead of
-# 'c++11'.
+# 'c++11',
 '-std=c99',
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
-'c++',
+'c',
+# 'c++',
 '-isystem',
 '/usr/include',
 '-isystem',
