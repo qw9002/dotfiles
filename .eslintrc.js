34753674e6bcde484a323232b2253f8d4ef9132e
qw9002 <1043102712@qq.com>
2019-10-30 14:04:50 +0800

    fix .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,23 @@ module.exports = {
   extends: 'eslint-config-alloy',
   // 添加规则
   plugins: [
-    '@typescript-eslint',
-    // 'typescript',
+    '@typescript-eslint'
+    // 'typescript'
   ],
   // 运行环境
   env: {
     browser: true, // 浏览器
     node: true,
-    es6: true,
+    es6: true
   },
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaVersion: 2017, // ECMA 2017 标准
     sourceType: 'module', // module 模式
-    ecmaFeatures: { jsx: true },
+    ecmaFeatures: { jsx: true }
   },
   // off: 0, warn: 1, error: 2
   rules: {
@@ -29,7 +29,7 @@ module.exports = {
       2, 'always', { null: 'ignore' }
     ],
     'no-undef': 2,
-    'no-unused-vars': 1,
+    'no-unused-vars': 1
   },
   overrides: [
     {
@@ -38,7 +38,7 @@ module.exports = {
         // 修复 ts 的问题：使用接口类型，但报未使用错误
         '@typescript-eslint/no-unused-vars': [
           2, { args: 'none' }
-        ],
+        ]
       }
     }
   ]
